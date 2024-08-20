3e6cce42b844417c8181d4ab08da8c1c9bf4c778
xuxing12 <2693787634@qq.com>
2024-08-20 21:21:18 +0800

    2

--- test.java
+++ test.java
@@ -3,7 +3,8 @@ public class test {
     public static void main(String[] args) {
         int x = 1;
         int y = 2;
+        int z = 3;
         
-        System.out.println(x+y);
+        System.out.println(x+y+z);
     }
 }
